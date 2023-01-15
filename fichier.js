17232ff5fab02422d5778aab9ce407dacf261672
Alexandre Mounier <alexandremounier14@gmail.com>
2023-01-15 11:18:29 +0100

    Squelette_HTML

---
+++ fichier.js
@@ -0,0 +1,9 @@
+/*let userInput = prompt("Enter a number:");
+
+multiplyByTwo(userInput);
+
+function multiplyByTwo(userInput) {
+  var result = userInput * 2;
+  alert("The result is: " + result);
+}
+*/
